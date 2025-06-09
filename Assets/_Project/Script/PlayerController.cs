605431e9aaef708e3b4b925374834c6157e1df39
JohnGhidot <marco.ghidotti2@gmail.com>
2025-06-09 21:54:47 +0200

    Creato CameraFollow e PlayController
    Dovrebbero essere completi ed a posto

--- Assets/_Project/Script/PlayerController.cs
+++ Assets/_Project/Script/PlayerController.cs
@@ -3,16 +3,31 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
+
 {
+    Rigidbody2D rb;
+    [SerializeField] private float _speed = 5f;
+    private float h;
+    private float v;
+    Vector2 Direction;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        h = Input.GetAxis("Horizontal");
+        v = Input.GetAxis("Vertical");
+    }
+
+    private void FixedUpdate()
+    {
+        Direction = new Vector2(h, v);
+        rb.MovePosition(rb.position + Direction *(_speed * Time.deltaTime));
     }
 }
