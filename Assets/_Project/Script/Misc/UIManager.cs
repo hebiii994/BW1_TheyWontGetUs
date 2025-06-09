723250eed1550a253fa5c435275823fe9d9369ef
hebiii994 <salvatoreguglielmini964@gmail.com>
2025-06-09 22:31:43 +0200

    Game Manager e UI Manager (da finire)

---
+++ Assets/_Project/Script/Misc/UIManager.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+    public void ShowGameOverScreen()
+    {
+        //implementare schermata GameOver
+    }
+
+    public void ShowGameScreen()
+    {
+        //mostriamo la schermata di gioco
+    }
+
+    public void ShowPauseScreen(bool pause)
+    {
+        //metti e togli la pausa
+    }
+
+    public void UpdateKills(int kills)
+    {
+        // incrementiamo il counter delle kill
+    }
+
+    public void UpdateTime(float time)
+    {
+        //incremento timer
+    }
+}
